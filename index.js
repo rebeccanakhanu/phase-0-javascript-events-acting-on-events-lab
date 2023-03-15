c6c471dc885cdb30b19c8eef15fff996aec50786
Wakhungu N Rebecca <rebeccanakhanu@gmail.com>
2023-03-15 16:31:05 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+function moveDodgerRight() {
+  const dodger = document.getElementById("dodger");
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    // stop moving right at the right edge of the game window
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+function moveDodgerLeft() {
+  const dodger = document.getElementById("dodger");
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    // stop moving left at the left edge of the game window
+    dodger.style.left = `${left - 1}px`;
+  }
+}
